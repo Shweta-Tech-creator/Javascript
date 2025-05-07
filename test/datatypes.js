3c394ee769801a32a551f7c5919e329548afb34c
Shweta-Tech-creator <kadamsweta92@gmail.com>
2025-05-07 15:34:24 +0000

    datatype

---
+++ test/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as a newer version
+
+// alert(3+3)   //we are using node.js not browser
+
+let name = "Shweta"
+let age = 19
+let loggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// String => ""
+// Boolean => true/false
+//  null => standalone value
+// undefined =>
+// symbol  => unique
+
+// object
+console.log (typeof("Shweta"))
+console.log (typeof(age))
+console.log (typeof(null))  //object
+console.log (typeof(undefined)) //undefined
